cbe9ffe7889278b66ea09258daeded77472188da
Paulo César Morandi Massuci <pcmassuci@gmail.com>
2019-11-12 21:54:58 -0300

    Commit inicial

---
+++ DND/DND/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DND
+//
+//  Created by Paulo César Morandi Massuci on 12/11/19.
+//  Copyright © 2019 Paulo César Morandi Massuci. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+    var ano: some View {
+        Text("World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
